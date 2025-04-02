eccdc67445e4ce358df99d08771db55243cdc89f
Brenden Eum <beum@caltech.edu>
2025-04-02 17:08:05 -0400

    batch script

--- job-train_alae.sh
+++ job-train_alae.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 #SBATCH --account=def-webbr
-#SBATCH --time=0-12:00:00 # DD-HH:MM:SS
+#SBATCH --time=0-08:00:00 # DD-HH:MM:SS
 #SBATCH --cpus-per-task=2
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:p100:1
 #SBATCH --mem=32000M
 #SBATCH --output=job-logs/train_alae-%j.out
+
+# Go to project folder
+cd scratch/ALAE
 
 # Load older modules
 module load StdEnv/2020
 module load python/3.8.10
 module load cuda/11.0
-export LD_LIBRARY_PATH=/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cudacore/11.0.2/lib/ 
-export CXXFLAGS="-D_GLIBCXX_USE_RANDOM_TR1"
-
-# Go to project folder
-cd projects/def-webbr/beum/ALAE/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cudacore/11.0.2/lib/"
 
 # Activate virtual environment for StyleALAE
 source env/bin/activate
-
-# Dataset prep
-# python -m dataset_preparation.prepare_evox
 
 # Train
 python train_alae.py -c evox
--- tutorial_using_cluster.sh
+++ tutorial_using_cluster.sh
@@ -1,23 +1,19 @@
 # Start an interactive session for debugging
 salloc --account=def-webbr  --time=04:00:00 --gres=gpu:1 --mem=16G --ntasks=1 --cpus-per-task=2
-salloc --account=def-webbr  --time=01:00:00 --gres=gpu:1 --mem=16G --ntasks=1 --cpus-per-task=1
+
 # Go to project folder
 # git clone https://github.com/BrendenEum/ALAE
-cd projects/def-webbr/beum/ALAE 
+#cd projects/def-webbr/beum/ALAE 
+cd scratch/ALAE 
 
 # Load older modules 
 module load StdEnv/2020
 module load python/3.8.10
 module load cuda/11.0
-export LD_LIBRARY_PATH="/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cudacore/11.1.1/lib/"
-export CXXFLAGS="-D_GLIBCXX_USE_RANDOM_TR1" 
-
-#export LD_LIBRARY_PATH="/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cudacore/11.0.2/lib/"
-#libcusolver.so.11 is in LD_LIBRARY_PATH="/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cudacore/11.1.1/lib/"
-#libcudnn.so.8 is in LD_LIBRARY_PATH="/cvmfs/soft.computecanada.ca/easybuild/software/2020/CUDA/cuda11.1/cudnn/8.2.0/lib/"
-
 # path to this cuda library
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cudacore/11.0.2/lib/"
 # random_device throws an error on cluster, tell it fall back to this pseudo-RNG
+# export CXXFLAGS="-D_GLIBCXX_USE_RANDOM_TR1" 
 
 # Make and activate venv
 # virtualenv --no-download env
