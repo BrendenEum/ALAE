0dc3f61a432ca6ab3f5094e44401bd555cfd7f12
Brenden Eum <beum@caltech.edu>
2025-04-02 15:41:47 -0400

    ignore

--- tutorial_using_cluster.sh
+++ tutorial_using_cluster.sh
@@ -1,6 +1,6 @@
 # Start an interactive session for debugging
 salloc --account=def-webbr  --time=04:00:00 --gres=gpu:1 --mem=16G --ntasks=1 --cpus-per-task=2
-
+salloc --account=def-webbr  --time=01:00:00 --gres=gpu:1 --mem=16G --ntasks=1 --cpus-per-task=1
 # Go to project folder
 # git clone https://github.com/BrendenEum/ALAE
 cd projects/def-webbr/beum/ALAE 
@@ -9,8 +9,13 @@ cd projects/def-webbr/beum/ALAE
 module load StdEnv/2020
 module load python/3.8.10
 module load cuda/11.0
-export LD_LIBRARY_PATH=/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cudacore/11.0.2/lib/ 
+export LD_LIBRARY_PATH="/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cudacore/11.1.1/lib/"
 export CXXFLAGS="-D_GLIBCXX_USE_RANDOM_TR1" 
+
+#export LD_LIBRARY_PATH="/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cudacore/11.0.2/lib/"
+#libcusolver.so.11 is in LD_LIBRARY_PATH="/cvmfs/soft.computecanada.ca/easybuild/software/2020/Core/cudacore/11.1.1/lib/"
+#libcudnn.so.8 is in LD_LIBRARY_PATH="/cvmfs/soft.computecanada.ca/easybuild/software/2020/CUDA/cuda11.1/cudnn/8.2.0/lib/"
+
 # path to this cuda library
 # random_device throws an error on cluster, tell it fall back to this pseudo-RNG
 
